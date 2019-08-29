b6fed58bd489f6a6443ccaa8dacc3bb833bad4cb
hidetoshi <hidetoshi.t@gmail.com>
2019-08-29 10:44:21 +0700

    change config

--- resources/linux/create_desktop_file.sh
+++ resources/linux/create_desktop_file.sh
@@ -5,14 +5,14 @@ THIS_PATH=`readlink -f $0`
 cd `dirname ${THIS_PATH}`
 FULL_PATH=`pwd`
 cd ${WORKING_DIR}
-cat <<EOS > Mattermost.desktop
+cat <<EOS > Gchat.desktop
 [Desktop Entry]
-Name=Mattermost
-Comment=Mattermost Desktop application for Linux
-Exec="${FULL_PATH}/mattermost-desktop"
+Name=Gchat
+Comment=Gchat Desktop application for Linux
+Exec="${FULL_PATH}/gchat-desktop"
 Terminal=false
 Type=Application
 Icon=${FULL_PATH}/icon.svg
 Categories=Network;InstantMessaging;
 EOS
-chmod +x Mattermost.desktop
+chmod +x Gchat.desktop
